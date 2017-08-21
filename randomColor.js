f9b819cf970794d300e0825c049cda71423ea9f8
Sasha011 <sasha_p10@hotmail.com>
2017-08-21 23:03:08 +0200

    Update randomColor.js

--- randomColor.js
+++ randomColor.js
@@ -1,5 +1,13 @@
+//Ovako u jednom fajlu to nije dobro
+//Validacije bi trebalo izdvojiti u poseban modul, to je samo jedan primer
+//Neke radnje se izvrsavaju i tek posle njih se proverava da li je unos validan?!
+//Zasto se koristi css funkcija jquery-ja kada je sve moguce raditi sa css klasama i stilovima?
+//Deklaracije i definicije promenljivih na pogresnim mestima
+//randomColor funkcija, ima po meni logicku manu, vidi komentare 
+
 $(document).ready(function(){
 				
+	//Zasto je definisano ovde
 	var colorArray = [];
 	
 	//After Form Submitted
@@ -14,21 +22,26 @@ $(document).ready(function(){
 		//split colors to array
 		colorArray = colors.split(/[,;]+/).filter(x => x);
 		
+		//Zasto je definisano ovde? Sam JS kompajler ce podici deklaraciju na vrh funkcije
 		var isColor = false;
 		for (var i = 0, len = colorArray.length; i < len; i++) {
 		  isColor = validTextColor(colorArray[i]);
 			if (!isColor) { break; }	
 		}
 		
+		//Zasto je definisano ovde? Sam JS kompajler ce podici deklaraciju na vrh funkcije
 		var atLeastTwoColors = colorArray.length > 1;
 		
 		if(isColor && atLeastTwoColors){inputColor.removeClass("invalid").addClass("valid");}
 		else{inputColor.removeClass("valid").addClass("invalid");}	
-
+		
+		//Zasto je definisano ovde? Sam JS kompajler ce podici deklaraciju na vrh funkcije
 		var error_free=true;
-	
+		
+		//Zasto je definisano ovde? Sam JS kompajler ce podici deklaraciju na vrh funkcije
 		var valid = inputColor.hasClass("valid");
 		
+		//Zasto je definisano ovde? Sam JS kompajler ce podici deklaraciju na vrh funkcije
 		var error_element = $("span", inputColor.parent());	
 		
 		if (!valid){
@@ -57,6 +70,9 @@ $(document).ready(function(){
 	function randomColor(colorArray){
 		var div = $('#colorParagraph p'); 
 		var chars = div.text().split('');
+		//nema potrebe za brisanjem
+		//$('#colorParagraph p').text().length je chars, a onda idi po indeksu i menjaj svaki karakter
+		//ti sad sve brises pa apendujes jedan po jedan, a to nije nikako optimalno
 		div.html('');     
 		var previousColor = -1;
 		for(var i=0; i<chars.length; i++) {
@@ -66,6 +82,9 @@ $(document).ready(function(){
 			}
 			else{
 				idx = getRandomColorIndex(colorArray);
+				
+				//Kad se selektuje random boja, treba da se izbaci iz niza, kako ne bi doslo do ponavljanja,
+				//a posle je treba vratiti u taj isti niz
 				
 				//check two neighboring letters to be different color
 				while ( idx == previousColor ){
